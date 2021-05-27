5b9167c8e42c225b57afacb88996bf4e4a55a628
luciano <lucianogrilli03@gmail.com>
2021-05-26 21:34:19 -0300

    mercadopago desarrollado y tambien el metodo comprar del navbar

--- src/app/services/mercadopagoDatos.service.ts
+++ src/app/services/mercadopagoDatos.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BASE_ENDPOINT } from '../config/app';
 import { MercadopagoDatos } from '../models/MercadopagoDatos';
+import { Pedido } from '../models/Pedido';
 import { Usuario } from '../models/usuario';
 import { CommonService } from './common.service';
 
@@ -15,5 +17,11 @@ export class MercadopagoDatosService extends CommonService<MercadopagoDatos>{
    }
 
   protected baseEndPoint = BASE_ENDPOINT + '/mercadopagoDatos';
+
+  getMPDPreferenceId(idUsuario: number, idPedido: number, pedido: Pedido): Observable<MercadopagoDatos>{
+    return this.http.post<MercadopagoDatos>(`${this.baseEndPoint}/checkoutmp/${idUsuario}/pedido/${idPedido}`,
+    pedido,
+    {headers: this.cabeceras});
+  }
 
 }
