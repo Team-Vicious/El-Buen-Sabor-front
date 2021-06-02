540a1431b26286657db9129daa3257c1516639cd
Joaquín Roza <joacoroza@gmail.com>
2021-06-02 20:01:31 -0300

    arreglé botones editar de articulo manufacturado

--- src/app/components/cocinero/manufacturados-form.component.ts
+++ src/app/components/cocinero/manufacturados-form.component.ts
@@ -18,6 +18,7 @@ export class ManufacturadosFormComponent implements OnInit {
   error: any;
   fotoSeleccionada!: File;
   usuarioId:any;
+  validadorFoto!: Number;
 
   constructor(
     private service: ArticuloManofacturadoService, 
@@ -39,7 +40,7 @@ export class ManufacturadosFormComponent implements OnInit {
   public crear(): void {
     this.service.crear(this.manufacturado).subscribe(manufacturado => {
       Swal.fire('Nuevo:', `Manufacturado ${manufacturado.denominacion} creado con éxito`, 'success');
-      this.router.navigate(['/manufacturados']);
+      this.volver();
     }, err => {
       if(err.status === 400){
         this.error = err.error;
@@ -53,7 +54,7 @@ export class ManufacturadosFormComponent implements OnInit {
     this.service.editar(this.manufacturado).subscribe(manufacturado => {
       console.log(manufacturado);
       Swal.fire('Modificado:', `Manufacturado ${manufacturado.denominacion} actualizado con éxito`, 'success');
-      this.router.navigate(['/manufacturados']);
+      this.volver();
     }, err => {
       if(err.status === 400){
         this.error = err.error;
@@ -75,8 +76,10 @@ export class ManufacturadosFormComponent implements OnInit {
   }
 
   public seleccionarFoto(event: any): void{
+    this.validadorFoto = 0;
     this.fotoSeleccionada = event.target.files[0];
     console.info(this.fotoSeleccionada);
+    this.validadorFoto = 1;
 
     if (this.fotoSeleccionada.type.indexOf('image') < 0) {
       Swal.fire('Error', 'El archivo deber del tipo imagen', 'error');
@@ -84,6 +87,8 @@ export class ManufacturadosFormComponent implements OnInit {
   }
 
   public crearConFoto(): void{
+    
+    if(this.validadorFoto==1){
     if(!this.fotoSeleccionada){
 
     }else{
@@ -99,10 +104,14 @@ export class ManufacturadosFormComponent implements OnInit {
         }
       })
 
-    }
+    }}
+    else{this.crear()}
   }
 
   public editarConFoto(): void{
+    
+    if(this.validadorFoto==1){
+      
     if(!this.fotoSeleccionada){
 
     }else{
@@ -118,7 +127,9 @@ export class ManufacturadosFormComponent implements OnInit {
         }
       })
 
-    }
+    }}
+
+    else{this.editar()}
   }
 }
 
