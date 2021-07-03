f03787c6fca8692587f235614c74f101ad604ff2
luciano <lucianogrilli03@gmail.com>
2021-07-02 21:16:11 -0300

    validaciones agregadas en rubros-form

--- src/app/components/admin/rubro-form/rubro-form.component.ts
+++ src/app/components/admin/rubro-form/rubro-form.component.ts
@@ -25,8 +25,8 @@ export class RubroFormComponent implements OnInit {
   rubroId!: number;
   rubrosArticulos: RubroArticulo[] = [];
 
-  constructor(private serviceRubroGeneral: RubroGeneralService, 
-    private serviceRubroArticulo: RubroArticuloService, 
+  constructor(private serviceRubroGeneral: RubroGeneralService,
+    private serviceRubroArticulo: RubroArticuloService,
     private router: Router,
     private route: ActivatedRoute,
     private location: Location) { }
@@ -37,9 +37,9 @@ export class RubroFormComponent implements OnInit {
     this.rubroId = +this.route.snapshot.paramMap.get('idr')!;
 
     //depende el tipo trae, utiliza un objeto o el otro y verifica si tiene id para editar sino crear
-    if(this.tipoRubro == 'general'){
-      if(this.rubroId){
-        this.serviceRubroGeneral.ver(this.rubroId).subscribe( rubro => {
+    if (this.tipoRubro == 'general') {
+      if (this.rubroId) {
+        this.serviceRubroGeneral.ver(this.rubroId).subscribe(rubro => {
           this.rubroGeneral = rubro;
         });
 
@@ -48,16 +48,16 @@ export class RubroFormComponent implements OnInit {
 
     }
 
-    if(this.tipoRubro == 'articulo'){
-      if(this.rubroId){
-        this.serviceRubroArticulo.ver(this.rubroId).subscribe( rubro => {
+    if (this.tipoRubro == 'articulo') {
+      if (this.rubroId) {
+        this.serviceRubroArticulo.ver(this.rubroId).subscribe(rubro => {
           this.rubroArticulo = rubro;
         });
 
       }
 
       //listo los rubros para asignarle un padre a rubro articulo despues
-      this.serviceRubroArticulo.listar().subscribe( rubros =>{
+      this.serviceRubroArticulo.listar().subscribe(rubros => {
         this.rubrosArticulos = rubros as RubroArticulo[];
         console.log("rubros traidos");
       });
@@ -65,70 +65,80 @@ export class RubroFormComponent implements OnInit {
       this.formArticulo = true;
 
     }
-    
-    
-  }
 
-  crearRubro(){
-    if(this.tipoRubro == 'general'){
-      this.serviceRubroGeneral.crear(this.rubroGeneral).subscribe( rubro => {
-        console.log("rubro general creado");
-        Swal.fire('CREADO!',`Rubro creado con exito`,'success');
-        this.volver();
-      });
-
-    }
-
-    if(this.tipoRubro == 'articulo'){
-      this.serviceRubroArticulo.crear(this.rubroArticulo).subscribe( rubro => {
-        console.log("rubro articulo creado");
-        Swal.fire('CREADO!',`Rubro creado con exito`,'success');
-        this.volver();
-      });
-
-    }
 
   }
 
-  editarRubro(){
-    if(this.tipoRubro == 'general'){
-      if(this.rubroId){
-        this.serviceRubroGeneral.editar(this.rubroGeneral).subscribe( rubro => {
-          console.log("rubro general editado");
-          Swal.fire('EDITADO!',`Rubro editado con exito`,'success');
+  crearRubro() {
+    if (this.rubroGeneral.denominacion || this.rubroArticulo.denominacion) {
+      if (this.tipoRubro == 'general') {
+        this.serviceRubroGeneral.crear(this.rubroGeneral).subscribe(rubro => {
+          console.log("rubro general creado");
+          Swal.fire('CREADO!', `Rubro creado con exito`, 'success');
           this.volver();
         });
 
       }
 
-    }
-
-    if(this.tipoRubro == 'articulo'){
-      if(this.rubroId){
-        this.serviceRubroArticulo.editar(this.rubroArticulo).subscribe( rubro => {
-          console.log("rubro articulo editado");
-          Swal.fire('EDITADO!',`Rubro editado con exito`,'success');
+      if (this.tipoRubro == 'articulo') {
+        this.serviceRubroArticulo.crear(this.rubroArticulo).subscribe(rubro => {
+          console.log("rubro articulo creado");
+          Swal.fire('CREADO!', `Rubro creado con exito`, 'success');
           this.volver();
         });
 
       }
+    } else {
+      Swal.fire('Error!', `Faltan llenar campos`, 'error');
 
     }
 
   }
 
-  asignarRubroPadre(rubroPadre: RubroArticulo){
+  editarRubro() {
+    if (this.rubroGeneral.denominacion || this.rubroArticulo.denominacion) {
+      if (this.tipoRubro == 'general') {
+        if (this.rubroId) {
+          this.serviceRubroGeneral.editar(this.rubroGeneral).subscribe(rubro => {
+            console.log("rubro general editado");
+            Swal.fire('EDITADO!', `Rubro editado con exito`, 'success');
+            this.volver();
+          });
+
+        }
+
+      }
+
+      if (this.tipoRubro == 'articulo') {
+        if (this.rubroId) {
+          this.serviceRubroArticulo.editar(this.rubroArticulo).subscribe(rubro => {
+            console.log("rubro articulo editado");
+            Swal.fire('EDITADO!', `Rubro editado con exito`, 'success');
+            this.volver();
+          });
+
+        }
+
+      }
+    } else {
+      Swal.fire('Error!', `Faltan llenar campos`, 'error');
+
+    }
+
+  }
+
+  asignarRubroPadre(rubroPadre: RubroArticulo) {
     this.rubroArticulo.padre = rubroPadre;
-    this.serviceRubroArticulo.editar(this.rubroArticulo).subscribe( rubro => {
+    this.serviceRubroArticulo.editar(this.rubroArticulo).subscribe(rubro => {
       console.log("rubro padre asignado-actualizado")
     });
   }
 
-  sacarRubroPadre(){
+  sacarRubroPadre() {
     var rubroAuxSinPadre: RubroArticulo = new RubroArticulo();
     //creo un rubro y le paso los valores pero sin padre y actualizo
     rubroAuxSinPadre.denominacion = this.rubroArticulo.denominacion
-    this.serviceRubroArticulo.editar(rubroAuxSinPadre).subscribe( rubro => {
+    this.serviceRubroArticulo.editar(rubroAuxSinPadre).subscribe(rubro => {
       console.log("rubro padre retirado-actualizado")
     });
   }
