7bdc67cfe87099e427140891d5045db3fa83ef8c
luciano <lucianogrilli03@gmail.com>
2021-07-29 20:10:52 -0300

    service modificado

--- src/app/services/articuloInsumo.service.ts
+++ src/app/services/articuloInsumo.service.ts
@@ -4,6 +4,7 @@ import { BASE_ENDPOINT } from '../config/app';
 import { ArticuloInsumo } from '../models/ArticuloInsumo';
 import { ArticuloManofacturadoService } from './articuloManofacturado.service';
 import { CommonService } from './common.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,21 @@ export class ArticuloInsumoService extends CommonService<ArticuloInsumo>{
    }
 
   protected baseEndPoint = BASE_ENDPOINT + '/articuloInsumo';
+
+  verNuevo(id: number){
+    return this.http.get(this.baseEndPoint + '/' + id).pipe(
+      map((res:any) => {
+        return res;
+      })
+    );
+  }
+
+  editarNuevo(articulo:ArticuloInsumo){
+    return this.http.put(this.baseEndPoint + '/' + articulo.id, articulo,{headers: this.cabeceras}).pipe(
+      map((res:any) => {
+        return res;
+      })
+    );
+  }
 
 }
